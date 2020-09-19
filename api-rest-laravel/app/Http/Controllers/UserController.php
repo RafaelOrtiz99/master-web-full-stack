664417e03ddff367279a3fc226c02e4bef54fe7d
Rafael <rortizr@unbosque.edu.co>
2020-09-18 22:12:33 -0500

    validar datos

--- api-rest-laravel/app/Http/Controllers/UserController.php
+++ api-rest-laravel/app/Http/Controllers/UserController.php
@@ -17,21 +17,55 @@ class UserController extends Controller
          */
         $json = $request->input('json', null);
         $params = json_decode($json); //objeto
-        $params_array = json_decode($json,true); //array       
+        $params_array = json_decode($json,true); //array  
+        
+    
+        if(!empty($params) && !empty($params_array)){
 
-        //Validar datos
+            //Limpiar datos
+            $params_array = array_map('trim', $params_array);
 
+            //Validar datos
+            $validate = \Validator::make($params_array,[
+                'name'     => 'required|alpha',
+                'surname'  => 'required|alpha',
+                'email'    => 'required|email',
+                'password' => 'required',
+            ]);
+
+            if($validate->fails()){
+                $data = [
+                    'status'  => 'error',
+                    'code'    => '404',
+                    'message' => 'User not created'
+                    ,'errors' => $validate->errors()
+                ];
+                return response()->json($data, 400);
+            }
+            else{
+                $data = [
+                    'status' => 'success',
+                    'code' => 200,
+                    'message' => 'User Saved'
+                ];
+            }
+        }
+        else{
+            $data = [
+                'status'  => 'error',
+                'code'    => '404',
+                'message' => 'Incorrect data',
+            ];
+
+        }
+
+        return response()->json($data, $data['code']);
+        
         //Cifrar contraseña
 
         //Comprobar si el usuario existe
 
         //Crear usuario
-
-       $data = [
-           'status' => 'error',
-           'code' => '404',
-           'message' => 'User not created'
-       ];
 
        return response()->json($data, $data['code']);
    }
